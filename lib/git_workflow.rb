51260954aac09011e6f19f5a56bbc87c6f640bfa
Simon Menke <simon.menke@gmail.com>
2010-11-27 13:08:45 +0100

    Make 'open' optional

--- lib/git_workflow.rb
+++ lib/git_workflow.rb
@@ -8,6 +8,9 @@ class GitWorkflow
   include Opts::DSL
 
   def feature(env, args)
+    if args.size == 1 and !%w( list open close update ).include?(args.first)
+      args.unshift "open"
+    end
     GitWorkflow::Feature.new.call(env, args)
   end
 

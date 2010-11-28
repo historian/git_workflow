a9f115fa9843decd062e4bd83b4432b52e9c9109
Simon Menke <simon.menke@gmail.com>
2010-11-28 15:49:01 +0100

    Rename feature# to topic#

--- lib/git_workflow.rb
+++ lib/git_workflow.rb
@@ -4,18 +4,26 @@ class GitWorkflow
 
   require 'git_workflow/version'
   require 'git_workflow/helpers'
-  require 'git_workflow/feature'
+  require 'git_workflow/topic'
+  require 'git_workflow/deployment'
 
   include Opts::DSL
 
-  def feature(env, args)
+  def topic(env, args)
     if args.empty?
       args << "list"
     end
     if args.size == 1 and !%w( list open close update ).include?(args.first)
       args.unshift "open"
     end
-    GitWorkflow::Feature.new.call(env, args)
+    GitWorkflow::Topic.new.call(env, args)
+  end
+
+  def deployment(env, args)
+    if args.empty?
+      args << "environments"
+    end
+    GitWorkflow::Deployment.new.call(env, args)
   end
 
 end
